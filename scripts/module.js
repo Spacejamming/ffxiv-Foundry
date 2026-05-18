75972a87a6a325b97fa4709939fdb79f63cf367b
Spacejamming <Ewan.gg01@gmail.com>
2026-05-18 12:52:11 +0100

    0.1.8

--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,7 @@
 import { FFXIVAoeManager } from "./aoe-manager.js";
 
 export const MODULE_ID = "ffxiv-vtt";
-export const MODULE_VERSION = "0.1.7";
+export const MODULE_VERSION = "0.1.8";
 let aoeManager;
 
 class FFXIVVTT {
